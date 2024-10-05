50a55e64c883743ef71adbb3c7c661a267814e9a
Ykpern <149092762+Ykpern@users.noreply.github.com>
2024-10-05 23:42:42 +0300

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,6 +1,7 @@
-
+#Only used for testing the regular expression not used in the main project
 import re
-
+regex = (r"^(0[1-9]|[1-7][0-9]|8[01])((\s?[a-zA-Z]\s?)(\d{4,5})|(\s?[a-zA-Z]{2}\s?)(\d{3,4})|(\s?[a-zA-Z]{3}\s?)(\d{2,"
+         r"3}))$")
 
 test_str = ("45 pww 354  ",
             "23 tt 4334  ",
@@ -9,8 +10,12 @@ test_str = ("45 pww 354  ",
 #matches = re.finditer(regex, test_str, re.MULTILINE)
 
 for i in test_str:
+            
     i = i.replace(" ", "")
-    if(True):
+            
+    if re.match(regex, i):
         print("Valid ")
     else:
         print("Invalid ")
+    
+        
